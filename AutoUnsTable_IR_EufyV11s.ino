749365495684f4f39c940ee7d12d10b46a957b9d
lees846 <lees846@newschool.edu>
2024-02-19 20:36:42 -0500

    Add select and send 2 random signals to UnsTable with 3 seconds between each set

--- AutoUnsTable_IR_EufyV11s.ino
+++ AutoUnsTable_IR_EufyV11s.ino
@@ -27,23 +27,95 @@
 #include "PinDefinitionsAndMore.h" // Define macros for input and output pin etc.
 #include <IRremote.hpp>
 
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+const String robotMoves[] = {"up", "right", "down", "left"};
 
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);   // Feedback on the arduino board builtin LED for when a signal is sent
     Serial.begin(115200);
-#if defined(__AVR_ATmega328PU__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/|| defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
-    delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
-#endif
-    // Just to know which program is running on my Arduino
-    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
-    Serial.println(F("Send IR signals at pin " STR(IR_SEND_PIN)));
+    
+    #if defined(__AVR_ATmega328PU__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/|| defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
+       delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
+    #endif
+    // Just to know which program is running on the Arduino
+    Serial.println(("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    Serial.println(("Send IR signals at pin " STR(IR_SEND_PIN)));
 
     IrSender.begin(); // Start with IR_SEND_PIN as send pin and enable feedback LED at default feedback LED pin
 }
 
 void loop() {
-  /**
-   * Sample IrSender code
+  // Select two random integers between 0 and length of robotMoves[]
+  const int firstMove = int(random(4));
+  const int secondMove = int(random(4));
+  Serial.println(firstMove);
+  Serial.println(secondMove);
+
+  // Move UnsTable depending on which index was chosen for each move
+  moveUnsTable(firstMove);
+  moveUnsTable(secondMove);
+
+  // Wait 3 seconds between each set of moves
+  delay(3000);
+
+}
+
+// Sends IR signal to UnsTable depending on which move is passed
+void moveUnsTable(int whichMove) {
+  if(robotMoves[whichMove] == "up"){
+    moveUp();
+  }  
+  else if(robotMoves[whichMove] == "right"){
+    turnRight();
+  }  
+  else if(robotMoves[whichMove] == "down"){
+    moveDown();
+  }
+  else if(robotMoves[whichMove] == "left"){
+    turnLeft();
+  }
+  
+  // add logic for each new element in robotMoves
+}
+
+// Send UP arrow signal to Eufy from IR LED
+void moveUp() {
+  Serial.println(("Send Eufy UP Command"));
+  Serial.flush();
+  uint32_t upRawData[]={0x70A8F416, 0x5D00};
+  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &upRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 0, 3);
+  delay(500); // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
+}
+
+// Send RIGHT arrow signal to Eufy from IR LED
+void turnRight() {
+  Serial.println(("Send Eufy RIGHT Command"));
+  Serial.flush();
+  uint32_t rightRawData[]={0xB4687616, 0x9800};
+  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &rightRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 200, 4);
+  delay(500);
+}
+
+// Send DOWN arrow signal to Eufy from IR LED
+void moveDown() {
+  Serial.println(("Send Eufy DOWN Command"));
+  Serial.flush();
+  uint32_t downRawData[]={0x34687E16, 0x1400};
+  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &downRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 500, 3);
+  delay(500);
+}
+
+// Send LEFT arrow signal to Eufy from IR LED
+void turnLeft() {
+  Serial.println(("Send Eufy LEFT Command"));
+  Serial.flush();
+  uint32_t leftRawData[]={0xD4687C16, 0xE700};
+  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &leftRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 500, 3);
+  delay(500);
+}
+
+
+/**
+   * Sample IrSender code (would be found in void loop(){})
    * IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &downRawData[0], 48, PROTOCOL_IS_LSB_FIRST, <RepeatPeriodMillis>, <numberOfRepeats>);
    * 
    * [0] to [6] are local to the remote, and can be used for all 4 tested signals for the Eufy (up right down left)
@@ -60,48 +132,3 @@ void loop() {
    * One send of forward/UP is around 200, 4 for any of the other button sends
    *  
   */
-  /*
-  Serial.println(F("Send Eufy UP Command"));
-  Serial.flush();
-  uint32_t genRawData[]={hex};
-  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &whichThing[index], 48, PROTOCOL_IS_LSB_FIRST, 0, 3);
-  delay(500); // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
-  */
-  
-  // Send UP arrow signal
-  Serial.println(F("Send Eufy UP Command"));
-  Serial.flush();
-  uint32_t upRawData[]={0x70A8F416, 0x5D00};
-  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &upRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 0, 3);
-  delay(500); // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
-  
-  // Send RIGHT arrow signal
-  Serial.println(F("Send Eufy RIGHT Command"));
-  Serial.flush();
-  uint32_t rightRawData[]={0xB4687616, 0x9800};
-  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &rightRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 200, 4);
-  delay(500);
-
-  // Send LEFT arrow signal
-  Serial.println(F("Send Eufy LEFT Command"));
-  Serial.flush();
-  uint32_t leftRawData[]={0xD4687C16, 0xE700};
-  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &leftRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 500, 3);
-  delay(500);
-
-  // Send DOWN arrow signal
-  Serial.println(F("Send Eufy DOWN Command"));
-  Serial.flush();
-  uint32_t downRawData[]={0x34687E16, 0x1400};
-  IrSender.sendPulseDistanceWidthFromArray(38, 3000, 2900, 550, 1400, 550, 450, &downRawData[0], 48, PROTOCOL_IS_LSB_FIRST, 500, 3);
-  delay(500);
-
-  delay(3000);
-
-}
-
-// array of moves[size] [up, right, down, left]
-// 
-
-// in loop
-  // 
